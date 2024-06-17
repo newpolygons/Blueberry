4b7d425ec0ad97e45be9a2b98fe295fa7234a9ff
NewPolygons <30321729+newpolygons@users.noreply.github.com>
2024-06-17 00:34:18 -0400

    resolve linux wallpaper issues

--- src/linux.py
+++ src/linux.py
@@ -1,4 +1,6 @@
 import os
 def applyWallpaperLinux():
-    cwd = os.getcwd()
-    os.system("gsettings set org.gnome.desktop.background picture-uri " + cwd + "/ImageCache/finalImage.png")
+    os.chdir('..')
+    print("Changing wallpaper")
+    os.system("gsettings set org.gnome.desktop.background picture-uri file://" + os.getcwd() + "/ImageCache/finalImage.png")
+    os.chdir('src')
